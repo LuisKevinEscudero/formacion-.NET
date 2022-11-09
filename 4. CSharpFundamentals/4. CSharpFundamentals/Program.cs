68f5b20fcfc7f066f6020c6de1d608a567e583a9
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-09 17:35:31 +0100

    ejer de objetos

---
+++ 4. CSharpFundamentals/4. CSharpFundamentals/Program.cs
@@ -0,0 +1,24 @@
+﻿using _4._CSharpFundamentals.Math;
+
+namespace CSharpFundamentals
+{
+
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.FirstName = "John";
+            person.LastName = "Smith";
+            person.Introduce();
+
+            Calculator calc = new Calculator();
+
+            var result = calc.Add(1, 2);
+            Console.WriteLine(result);
+
+        }
+    }
+}
+
+

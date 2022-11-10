6bddce18e072d36e212a3c55b8fe6b65c1326eb7
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-10 11:25:54 +0100

    ej5 terminado

---
+++ ej5-DivisibleByThree/ej5-DivisibleByThree/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a program to count how many numbers between 
+1 and 100 are divisible by 3 with no remainder. 
+Display the count on the console.
+ */
+
+namespace DivisibleByThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count=0;
+
+            for(int i=0;i<100;i++)
+            {
+                if(i % 3 ==0)
+                {
+                    count++;
+                    Console.WriteLine("{0} is divisible by 3",i);
+                }
+                
+            }
+            Console.WriteLine("************");
+            Console.WriteLine("{0} are divible by 3 in 0-100", count);
+        }
+    }
+}

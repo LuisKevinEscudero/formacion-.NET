73b93b2a00c90aeb26745a75c6a9cd05715c3316
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-10 09:27:44 +0100

    ej1 echo

---
+++ 9. CSharpFundamentals6/9. CSharpFundamentals6/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MyNamespace
+{
+	public class Person
+	{
+		public int Age;
+	}
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            var person = new Person() { Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
+        }
+    }
+}
---
+++ ej1-ValidNumber/ej1-ValidNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ Write a program and ask the user to enter a number. 
+The number should be between 1 to 10. If the user 
+enters a valid number, display "Valid" on the console. 
+Otherwise, display "Invalid". (This logic is used a 
+lot in applications where values entered into input 
+boxes need to be validated).
+ */
+
+namespace ValidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number between 1 and 10: ");
+                string input = Console.ReadLine();
+                int number;
+
+                int result = Convert.ToInt32(input);
+                if (result >= 1 && result <= 10 )
+                {
+                    Console.WriteLine("Valid");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid");
+            }
+            
+        }
+    }
+}

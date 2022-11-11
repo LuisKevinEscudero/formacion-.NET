3826140fc28d2b0bc126fc0f35fb6c57bbe43598
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-11 16:20:06 +0100

    ej14 completado

---
+++ ej14-DisplayUniqueNumbers/ej14-DisplayUniqueNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Write a program and ask the user to continously 
+enter a number or type "Quit" to exit. The list 
+of numbers may include duplicates. Display the 
+unique numbers that the user has entered. 
+ */
+
+namespace DisplayUniqueNumbers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            while (true)
+            {
+                Console.WriteLine("Enter a number or type 'Quit' to exit: ");
+                var input = Console.ReadLine();
+                input = input.ToLower();
+                if (input == "quit")
+                    break;
+                numbers.Add(Convert.ToInt32(input));
+            }
+            var uniqueNumbers = new List<int>();
+            foreach (var number in numbers)
+            {
+                if (!uniqueNumbers.Contains(number))
+                    uniqueNumbers.Add(number);
+            }
+            Console.WriteLine("Unique numbers: ");
+            foreach (var number in uniqueNumbers)
+                Console.WriteLine(number);
+        }
+    }
+}

dd3460521b34842ae5922f8c572e42e16a970b6b
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-11 09:32:04 +0100

    ej11 facil

---
+++ ej11-ReverseGivenName/ej11-ReverseGivenName/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Write a program and ask the user to enter their name. 
+Use an array to reverse the name and then store the 
+result in a new string. Display the reversed name on the console. 
+ */
+
+namespace ReverseGivenName
+{
+	public class Programm
+	{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            var name = Console.ReadLine();
+            var array = new char[name.Length];
+            for (int i = 0; i < name.Length; i++)
+            {
+                array[i] = name[name.Length - 1 - i];
+            }
+
+            var reverse = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                reverse += array[i];
+            }
+            Console.WriteLine(reverse);
+
+        }
+    }
+}

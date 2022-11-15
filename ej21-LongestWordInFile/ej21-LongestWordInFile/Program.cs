a4a5f61f7bb632a131149a9e760f32c6167925a3
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-15 09:41:01 +0100

    ej21 normalito

---
+++ ej21-LongestWordInFile/ej21-LongestWordInFile/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Write a program that reads a file and displays the longest word in the file. 
+ */
+namespace LongestWordInFile
+{
+	public class Programm
+	{
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\luiskevin.escudero\Desktop\formacion .NET/words.txt";
+            string[] lines = File.ReadAllLines(path);
+            string longestWord = "";
+            foreach (string line in lines)
+            {
+                string[] words = line.Split(' ');
+                foreach (string word in words)
+                {
+                    if (word.Length > longestWord.Length)
+                    {
+                        longestWord = word;
+                    }
+                }
+            }
+            Console.WriteLine("Longest word: " + longestWord);
+        }
+    }
+}

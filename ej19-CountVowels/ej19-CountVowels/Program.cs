4ff1a2da057df4c22d1c8b8807e4d1573f0463c4
luiskevin.escudero <luiskevin.escudero@bosonit.com>
2022-11-15 09:15:30 +0100

    ej19 muy facil

---
+++ ej19-CountVowels/ej19-CountVowels/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Write a program and ask it to enter an English word. 
+Count the nunber of vowels (a, e, i, o, u) in the word. 
+So, if the user enters "inadequate", the program should 
+display 6 in the console.
+ */
+
+namespace CountVowels
+{
+	public class Programm
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Enter a word: ");
+            var input = Console.ReadLine().ToLower();
+			int count = 0;
+			for (int i = 0; i < input.Length; i++)
+			{
+				if (input[i].Equals('a') || input[i].Equals('e') || input[i].Equals('i') || input[i].Equals('o') || input[i].Equals('u'))
+				{
+					count++;
+				}
+			}
+			Console.WriteLine("The number of vowels are: "+count);
+        }
+	}
+}
